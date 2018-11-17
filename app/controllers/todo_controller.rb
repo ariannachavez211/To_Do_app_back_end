2a3c6f94becc84bb9a174dfc068ce58d85e01df5
Arianna Chavez <ariannaisabella44@gmail.com>
2018-11-17 17:20:56 +0000

    pomodoro estimate task pages add

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,8 +1,20 @@
 class TodoController < ApplicationController
     def index
     end
-    def show
-        @todo_description = "make it clean"
-        @todo_pomodoro_estimate = 4
+    def show 
+        todo_id=params[:id]
+        if todo_id == "1"
+            @todo_description = "make it clean"
+            @todo_pomodoro_estimate = 4
+        elsif todo_id == "2"
+            @todo_description = "sparkle"
+            @todo_pomodoro_estimate = 2
+        elsif todo_id == "3"
+            @todo_description = "shiny"
+            @todo_pomodoro_estimate = 1
+        elsif todo_id == "4"
+            @todo_description = "gleam"
+            @todo_pomodoro_estimate = 6
+        end
     end
 end
