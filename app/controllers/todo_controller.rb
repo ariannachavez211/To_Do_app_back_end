b3bb922206e26b0117e926cbdcb43dd09ea5745a
Arianna Chavez <ariannaisabella44@gmail.com>
2018-11-18 15:11:34 +0000

    todo create, route add

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,28 +2,15 @@ class TodoController < ApplicationController
     def index
     end
     def show 
-        todo_id=params[:id]
-        if todo_id == "1"
-            @todo_description = "make it clean"
-            @todo_pomodoro_estimate = 4
-        elsif todo_id == "2"
-            @todo_description = "yummy"
-            @todo_pomodoro_estimate = 2
-        elsif todo_id == "3"
-            @todo_description = "BTS"
-            @todo_pomodoro_estimate = 1
-        elsif todo_id == "4"
-            @todo_description = "sqeaky clean"
-            @todo_pomodoro_estimate = 3
-        elsif todo_id == "5"
-            @todo_description = "scrub scrub"
-            @todo_pomodoro_estimate = 2
-         elsif todo_id == "6"
-            @todo_description = "swipe and wipe"
-            @todo_pomodoro_estimate = 2
-            elsif todo_id == "7"
-            @todo_description = "BANG! TAN! SOYEON! DAN!"
-            @todo_pomodoro_estimate = 2
-        end
+        @todo=Todo.find_by_id(params[:id])
     end
+    def new 
+    end
+    def create
+         t = Todo.new
+         t.description = params['description']
+         t.pomodoro_estimate = params['pomodoro_estimate']
+         t.save
+         redirect_to "/todo/show/#{ t.id }"
+        end
 end
